4947d3dbd3d7e24f7cce014bbdf0900d7c7e8037
Dumitru MUNTEANU <dumitru.munteanu.97@mail.ru>
2021-12-18 23:45:13 +0200

    feat: add posts and comments migrations for blog page

---
+++ database/migrations/2021_12_18_085728_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->foreignId('user_id')->constrained('users')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+            $table->string('image');
+            $table->dateTime('published_at')->nullable();
+            $table->tinyText('excerpt');
+            $table->string('seo_title');
+            $table->string('seo_description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
