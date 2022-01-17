868cc7f015cecb9e08ab66fc64c0b6369f0bd5f4
Dumitru MUNTEANU <dumitru.munteanu.97@mail.ru>
2022-01-17 16:42:05 +0200

    feat: integrate logs in PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,15 +6,20 @@ use App\Http\Request\PostRequest;
 use App\Models\Post;
 use App\Services\PostCreator;
 use Illuminate\Http\Request;
+use Psr\Log\LoggerInterface;
 
 class PostController extends Controller {
     public function newPost() {
         return view('blog.new_post');
     }
 
-    public function showPost($id) {
+    public function showPost($id, Request $request, LoggerInterface $logger) {
         $post = Post::findOrFail($id);
         $comments = Post::findOrFail($id)->comments()->orderBy('published_at', 'DESC')->paginate(5);
+
+        $user = $request->user();
+        $userRepresentation = $user ? "User with id {$user->id}" : 'Unknown user';
+        $logger->info($userRepresentation . ' accessed article with id ' . $id, ['in' => $id, 'title' => $post->title]);
 
         return view('blog.post.post', [
             'post' => $post,
