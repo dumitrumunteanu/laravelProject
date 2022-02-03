c30de859a9e0b2b7fa661b427de3f8e20100effe
Dumitru MUNTEANU <dumitru.munteanu.97@mail.ru>
2022-02-03 19:53:25 +0200

    fix: load calendar scripts based on route

--- resources/views/layouts/app_logged_in.blade.php
+++ resources/views/layouts/app_logged_in.blade.php
@@ -20,11 +20,13 @@
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="{{ asset('css/calendar.css') }}">
-    <script src="{{ asset('js/calendar.js') }}"></script>
+    @if(request()->routeIs('calendar'))
+        <link rel="stylesheet" href="{{ asset('css/calendar.css') }}">
+        <script src="{{ asset('js/calendar.js') }}"></script>
 
-    <link rel="stylesheet" href="{{ asset('css/calendarUI.css') }}">
-    <script src="{{ asset('js/calendarUI.js') }}"></script>
+        <link rel="stylesheet" href="{{ asset('css/calendarUI.css') }}">
+        <script src="{{ asset('js/calendarUI.js') }}"></script>
+    @endif
 </head>
 <body class="bg-light" style="margin-left: 4.5rem">
 <div id="app">
