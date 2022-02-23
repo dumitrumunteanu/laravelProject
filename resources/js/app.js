0aac8938fbf5ec181730d6cd1163236f4b3d6aa4
Dumitru MUNTEANU <dumitru.munteanu.97@mail.ru>
2022-02-23 16:52:47 +0200

    fix: bootstrap not defined error

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,8 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+import * as bootstrap from "bootstrap";
+window.bootstrap = bootstrap;
 
 window.Vue = require('vue').default;
 
--- resources/views/layouts/app_logged_in.blade.php
+++ resources/views/layouts/app_logged_in.blade.php
@@ -10,15 +10,14 @@
     <title>{{ config('app.name', 'Organizer') }}</title>
 
     <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <script src="https://kit.fontawesome.com/aac533ff8d.js" crossorigin="anonymous"></script>
 
     <!-- Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     @if(request()->routeIs('calendar'))
         <link rel="stylesheet" href="{{ asset('css/calendarUI.css') }}">
